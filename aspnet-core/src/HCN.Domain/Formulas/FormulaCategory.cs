95d69412d229121fd8f7b72b76daf514748513ed
Holwn <ltienhoang2@gmail.com>
2024-01-25 11:45:18 +0700

    add: Visibility in FormulaCategory Entity

--- aspnet-core/src/HCN.Domain/Formulas/FormulaCategory.cs
+++ aspnet-core/src/HCN.Domain/Formulas/FormulaCategory.cs
@@ -12,6 +12,7 @@ namespace HCN.Formulas
         public string Slug { get; set; }
         public string CoverPicture { get; set; }
         public string Description { get; set; }
+        public bool Visibility { get; set; }
         public string KeywordSEO { get; set; }
         public string DescriptionSEO { get; set; }
         public Guid? ParentId { get; set; }
---
+++ aspnet-core/src/HCN.EntityFrameworkCore/Migrations/20240125044333_AddVisibilityInTableFormulaCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCN.Migrations
+{
+    public partial class AddVisibilityInTableFormulaCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Visibility",
+                table: "HCNFormulaCategories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visibility",
+                table: "HCNFormulaCategories");
+        }
+    }
+}
